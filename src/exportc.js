d2294c66ee6558a93341d4074f50b90c5644a2bd
fudesign2008 <fudesign2008@163.com>
2018-09-10 10:23:45 +0800

    modified:   UltiSnips/javascript.snippets         new file:   src/exportc.js

---
+++ src/exportc.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author fuyg
+ * @date  `date +%Y-%m-%d`
+ */
+
+class Child extends SuperClass {
+  constructor(options) {
+    super(options)
+    // TODO
+  }
+}
+
+export default Child
